092f83a144a0de8a2d30acfdaedcdb48213e8d98
Dan Lyons <danielclyons@yahoo.com>
2020-11-12 18:42:08 -0500

    added user.belongsTo.quotes

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,9 @@
 const User = require("./User");
 const Quotes = require("./Quotes");
 
-// User.hasMany(Quotes, {
-//     foreignKey: 'quotes_id'
-// })
+User.belongsTo(Quotes, {
+    foreignKey: 'quotes_id'
+})
 
 Quotes.hasMany(User, {
     foreignKey: 'quotes_id'
