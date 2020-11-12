e5ba1e1f7a722a907d8c0c6842ef75778092a7bc
Dan Lyons <danielclyons@yahoo.com>
2020-11-12 18:52:44 -0500

    changed to belongsToMany

--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,16 @@
 const User = require("./User");
 const Quotes = require("./Quotes");
+const User_Quotes = require("./User_Quotes");
 
-User.belongsTo(Quotes, {
+User.belongsToMany(Quotes, {
+    through: User_Quotes,
+    as: 'user-and-quotes',
     foreignKey: 'quotes_id'
 })
 
-Quotes.hasMany(User, {
+Quotes.belongsToMany(User, {
+    through: User_Quotes,
+    as: 'user-and-quotes',
     foreignKey: 'quotes_id'
 })
 
