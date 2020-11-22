fa94a9269f12667a48e50648b2101ef95fe4f8d5
Dan Lyons <danielclyons@yahoo.com>
2020-11-21 20:10:29 -0500

    working on manual mysql database

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,3 +1,34 @@
-DROP DATABASE IF EXISTS site_inspiration;
+-- DROP DATABASE IF EXISTS site_inspiration;
 
-CREATE DATABASE site_inspiration;
+-- CREATE DATABASE site_inspiration;
+
+DROP DATABASE site_inspiration_manual;
+
+CREATE DATABASE site_inspiration_manual;
+USE site_inspiration_manual;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS reason;
+DROP TABLE IF EXISTS quotes;
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(30),
+  email VARCHAR(30),
+  quotes_id INTEGER,
+  user_password DECIMAL,
+  CONSTRAINT fk_quotes FOREIGN KEY (quotes_id) REFERENCES quotes(id) ON DELETE SET NULL
+);
+
+CREATE TABLE reason (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  reason_tag VARCHAR(30),
+  quotes_id INTEGER,
+  CONSTRAINT fk_quotes FOREIGN KEY (quotes_id) REFERENCES quotes(id) ON DELETE SET NULL
+);
+
+CREATE TABLE quotes (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  author VARCHAR(30),
+  quote VARCHAR(30),
+);
