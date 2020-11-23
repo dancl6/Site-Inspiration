338249efc1f4e0261463da13c2fc8a84e9fe05e7
Dan Lyons <danielclyons@yahoo.com>
2020-11-22 20:25:58 -0500

    updated schema.sql to create manual mysql database, in progress

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,8 +1,4 @@
--- DROP DATABASE IF EXISTS site_inspiration;
-
--- CREATE DATABASE site_inspiration;
-
-DROP DATABASE site_inspiration_manual;
+DROP DATABASE IF EXISTS site_inspiration_manual;
 
 CREATE DATABASE site_inspiration_manual;
 USE site_inspiration_manual;
@@ -15,20 +11,20 @@ CREATE TABLE users (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   username VARCHAR(30),
   email VARCHAR(30),
-  quotes_id INTEGER,
-  user_password DECIMAL,
-  CONSTRAINT fk_quotes FOREIGN KEY (quotes_id) REFERENCES quotes(id) ON DELETE SET NULL
+  user_password DECIMAL
 );
 
 CREATE TABLE reason (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  reason_tag VARCHAR(30),
-  quotes_id INTEGER,
-  CONSTRAINT fk_quotes FOREIGN KEY (quotes_id) REFERENCES quotes(id) ON DELETE SET NULL
+  reason_tag VARCHAR(30)
 );
 
 CREATE TABLE quotes (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   author VARCHAR(30),
   quote VARCHAR(30),
+  reason_id INTEGER,
+  users_id INTEGER,
+  CONSTRAINT fk_reason FOREIGN KEY (reason_id) REFERENCES reason(id) ON DELETE SET NULL,
+  CONSTRAINT fk_users FOREIGN KEY (users_id) REFERENCES users(id) ON DELETE SET NULL
 );
