0ede4da1040ac9a4272176685fb53a63af8d2ef4
Dan Lyons <danielclyons@yahoo.com>
2020-12-18 21:55:41 -0500

    jwt in progress

--- server/controllers/api/user-routes.js
+++ server/controllers/api/user-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { User } = require('../../models');
 const passport = require('../../utils/passport');
 const isAuth = require('../../utils/middleware/isAuth');
+const { signToken } = require('../../utils/auth')
 
 // GET all users
 router.get('/', (req, res) => {
